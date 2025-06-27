4ac9a67ac00b7b10e3b9ca4a82f53d8b81e15fa3
Julian-Banks <julianrowlandbanks@gmail.com>
2025-06-27 17:48:25 +0200

    updating git ignore

--- build/lib/photobooth/config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-HOME = Path.home()
-
-POSE_LITE = HOME / "/Users/julianbanks/Documents/ThingKing/photobooth/assets/models/pose_landmarker_lite.task"
-POSE_FULL =  HOME / "/Users/julianbanks/Documents/ThingKing/photobooth/assets/models/pose_landmarker_full.task"
-POSE_HEAVY = HOME / "/Users/julianbanks/Documents/ThingKing/photobooth/assets/models/pose_landmarker_heavy.task"
-OVERLAY = HOME / "/Users/julianbanks/Documents/ThingKing/photobooth/assets/overlays/overlay.png"
