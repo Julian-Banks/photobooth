4d7bb9b30e2d4f3a378a0e22a78d688a78b4fde7
Julian-Banks <julianrowlandbanks@gmail.com>
2025-07-15 15:11:36 +0200

    Finishing fron overlay

--- src/main.py
+++ src/main.py
@@ -1,83 +1,150 @@
 from photobooth import camera, pose_detection, drawing
 from flask import Flask, Response, render_template
-import json 
+import json
 import time
 import argparse
 
-app = Flask(__name__,static_folder = 'web/static', template_folder ='web/templates')
+app = Flask(
+    __name__, static_folder='web/static', template_folder='web/templates'
+)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/stats_feed')
 def stats_feed():
     def generate():
         while True:
-            stats ={
+            stats = {
                 "fps": pose_detection.DETECTION_STATE.fps,
-                "model": ["lite","full","heavy"][app.config["MODEL"]],
-                "numPoses": app.config["numPoses"]
+                "model": ["lite", "full", "heavy"][app.config["MODEL"]],
+                "numPoses": app.config["numPoses"],
             }
             json_data = json.dumps(stats)
             yield f"data: {json_data}\n\n"
             time.sleep(0.1)
-    
-    return Response(generate(), mimetype = 'text/event-stream')
+
+    return Response(generate(), mimetype='text/event-stream')
+
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(main_loop(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        main_loop(), mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
 
 def main_loop():
     print("starting stream")
     stream = camera.start_stream()
     print("loading landmarker")
-    landmarker = pose_detection.setup_pose_landmarker(model = app.config['MODEL'], num_poses=app.config['numPoses'], enable_segmentation= app.config['BACKGROUND'])
-    try: 
+    landmarker = pose_detection.setup_pose_landmarker(
+        model=app.config['MODEL'],
+        num_poses=app.config['numPoses'],
+        enable_segmentation=app.config['BACKGROUND'],
+    )
+    try:
         run = True
-        while run :
-            ret,frame = stream.read()
-            pose_detection.detect_pose(landmarker = landmarker, frame = frame)
-            frame = drawing.process_image(frame, skeleton=app.config['SKELETON'], landmark_no= app.config['LANDMARK'], background = app.config['BACKGROUND'], overlay=app.config['OVERLAY'])
+        while run:
+            ret, frame = stream.read()
+            pose_detection.detect_pose(landmarker=landmarker, frame=frame)
+            frame = drawing.process_image(
+                frame,
+                skeleton=app.config['SKELETON'],
+                landmark_no=app.config['LANDMARK'],
+                background=app.config['BACKGROUND'],
+                overlay=app.config['OVERLAY'],
+                pickachu=app.config['PIKACHU'],
+            )
             MJPEG_stream = drawing.get_stream_frame(frame)
-            #run = camera.display_stream(frame)
+            # run = camera.display_stream(frame)
             yield MJPEG_stream
-    finally:        
+    finally:
         camera.shutdown(stream)
+
 
 def create_arg_parser():
     parser = argparse.ArgumentParser(description="Motion tracking overlay")
-    parser.add_argument("-l","--landmark", type=int,default = 12, help = "Index of the pose landmark to track (12 for shoulder, 15 for wrist")
-    parser.add_argument("-s","--skeleton", type=int, default = 0, help = "Show the skeleton for motion tracking with 1 (defualt) or 0 to hide it")
-    parser.add_argument("-p","--numPoses", type=int, default = 1, help='Max number of poses that can be detected by the landmarker', required=False)
-    parser.add_argument("-m","--model", type=int, default = 0, help= "0 - Pose detector lite, 1 - pose detector full, 2 - pose detector heavy")
-    parser.add_argument("-b", "--background", type = int, default = 1, help = "0 - normal live stream background, 1 for remove background" )
-    parser.add_argument("-o","--overlay", type= int, default=1, help = "0 - no front overlay, 1 for a static front overlay")
+    parser.add_argument(
+        "-l",
+        "--landmark",
+        type=int,
+        default=12,
+        help="Index of the pose landmark to track (12 for shoulder, 15 for wrist",
+    )
+    parser.add_argument(
+        "-s",
+        "--skeleton",
+        type=int,
+        default=0,
+        help="Show the skeleton for motion tracking with 1 (defualt) or 0 to hide it",
+    )
+    parser.add_argument(
+        "-p",
+        "--numPoses",
+        type=int,
+        default=1,
+        help='Max number of poses that can be detected by the landmarker',
+        required=False,
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=int,
+        default=0,
+        help="0 - Pose detector lite, 1 - pose detector full, 2 - pose detector heavy",
+    )
+    parser.add_argument(
+        "-b",
+        "--background",
+        type=int,
+        default=1,
+        help="0 - normal live stream background, 1 for remove background",
+    )
+    parser.add_argument(
+        "-o",
+        "--overlay",
+        type=int,
+        default=1,
+        help="0 - no front overlay, 1 for a static front overlay",
+    )
+    parser.add_argument(
+        "-pk",
+        "--pikachu",
+        type=int,
+        default=0,
+        help="1 for pikachu, 0 for flower",
+    )
+
     return parser
 
-    
+
 if __name__ == "__main__":
-    parser = create_arg_parser()  
+    parser = create_arg_parser()
     args = parser.parse_args()
-    
+
     app.config['LANDMARK'] = args.landmark
     if args.skeleton == 1:
         app.config['SKELETON'] = True
     else:
         app.config['SKELETON'] = False
-    
     app.config['numPoses'] = args.numPoses
-    app.config['MODEL']    = args.model
-    if args.background == 1: 
+    app.config['MODEL'] = args.model
+    if args.background == 1:
         app.config['BACKGROUND'] = True
-    else: 
-        app.config['BACKGROUND'] = False 
-    if args.overlay ==1:
+    else:
+        app.config['BACKGROUND'] = False
+    if args.overlay == 1:
         app.config['OVERLAY'] = True
     else:
         app.config['OVERLAY'] = False
 
-    app.run(debug = True, host ='0.0.0.0', port = '8080')
-    
-  
+    if args.pikachu == 1:
+        app.config['PIKACHU'] = True
+    else:
+        app.config['PIKACHU'] = False
+
+    app.run(debug=True, host='0.0.0.0', port='8080')
--- src/photobooth/pose_detection.py
+++ src/photobooth/pose_detection.py
@@ -1,15 +1,19 @@
 import re
 import mediapipe as mp
 import time
-from mediapipe.tasks.python.vision import PoseLandmarkerOptions, PoseLandmarkerResult
+from mediapipe.tasks.python.vision import (
+    PoseLandmarkerOptions,
+    PoseLandmarkerResult,
+)
 import numpy as np
-from photobooth.config import POSE_FULL , POSE_HEAVY, POSE_LITE 
+from photobooth.config import POSE_FULL, POSE_HEAVY, POSE_LITE
 
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
 PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
+
 
 class DetectionState:
     def __init__(self):
@@ -20,20 +24,24 @@ class DetectionState:
         self.output_image = None
 
     def update(self, result, output_image):
-        if self.counter%10 ==0 :
-            self.fps = 10/(time.time()-self.start_time)
+        if self.counter % 10 == 0:
+            self.fps = round(10 / (time.time() - self.start_time), 2)
             self.start_time = time.time()
         self.result = result
         self.counter += 1
         self.output_image = output_image
 
+
 DETECTION_STATE = DetectionState()
 
-def setup_pose_landmarker(model:int, num_poses:int, enable_segmentation:bool):
-    match model: 
+
+def setup_pose_landmarker(
+    model: int, num_poses: int, enable_segmentation: bool
+):
+    match model:
         case 0:
             model_path = POSE_LITE
-        case 1: 
+        case 1:
             model_path = POSE_FULL
             print(f"Loading model from {POSE_FULL}")
             print(f"Model exists? {POSE_FULL.exists()}")
@@ -41,25 +49,29 @@ def setup_pose_landmarker(model:int, num_poses:int, enable_segmentation:bool):
             model_path = POSE_HEAVY
         case _:
             model_path = POSE_LITE
-            raise RuntimeWarning("No pose detection model was selected. Defaulting to lite")
-   
+            raise RuntimeWarning(
+                "No pose detection model was selected. Defaulting to lite"
+            )
+
     options = PoseLandmarkerOptions(
-        base_options = BaseOptions(model_asset_path = model_path),
-        running_mode = VisionRunningMode.LIVE_STREAM,
-        num_poses = num_poses,
-        output_segmentation_masks = enable_segmentation,
-        result_callback = detection_result,
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        num_poses=num_poses,
+        output_segmentation_masks=enable_segmentation,
+        result_callback=detection_result,
     )
     landmarker = PoseLandmarker.create_from_options(options)
     return landmarker
 
-def detect_pose(landmarker:PoseLandmarker, frame):
-    #convert frame into right format for mediapipe. Settings from the example and I havne't had any issues so far.
-    mp_frame = mp.Image(image_format = mp.ImageFormat.SRGB, data = frame)
-    frame_timestamp_ms = time.monotonic_ns()//1000
+
+def detect_pose(landmarker: PoseLandmarker, frame):
+    # convert frame into right format for mediapipe. Settings from the example and I havne't had any issues so far.
+    mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    frame_timestamp_ms = time.monotonic_ns() // 1000
     landmarker.detect_async(mp_frame, frame_timestamp_ms)
 
-def detection_result(result:PoseLandmarkerResult, output_image:mp.Image, timestamp_ms:int):
+
+def detection_result(
+    result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int
+):
     DETECTION_STATE.update(result, output_image)
-
-
